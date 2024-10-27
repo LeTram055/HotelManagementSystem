c78efa69228cb89d6aab8823e107551864b587a1
letram055 <tramb2105564@student.ctu.edu.vn>
2024-10-27 22:56:21 +0700

    cap nhat

--- resources/views/admin/roomstatus/index.blade.php
+++ resources/views/admin/roomstatus/index.blade.php
@@ -12,8 +12,8 @@ Quản lý trạng thái phòng
 <div class="flash-message">
     @foreach (['danger', 'warning', 'success', 'info'] as $msg)
     @if(Session::has('alert-' . $msg))
-    <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert"
-            aria-label="close">&times;</a></p>
+    <p class="alert alert-{{ $msg }}">{{ Session::get('alert-' . $msg) }} <button type="button" class="btn-close"
+            data-bs-dismiss="alert" aria-label="Close"></p>
     @endif
     @endforeach
 </div>
