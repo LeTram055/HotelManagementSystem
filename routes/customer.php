dafc996a2aaabb518d36ac36904f9a53d0247435
letram055 <tramb2105564@student.ctu.edu.vn>
2024-11-21 20:11:28 +0700

    chuc nang dat phong cho khach hang

--- routes/customer.php
+++ routes/customer.php
@@ -2,6 +2,7 @@
     use Illuminate\Support\Facades\Route;
     use App\Http\Controllers\Customer\TypeController;
     use App\Http\Controllers\Customer\CustomerController;
+    use App\Http\Controllers\Customer\ReservationController;
 
 //loai phong
 Route::get('customer/types', [TypeController::class, 'index']);
@@ -11,3 +12,7 @@ Route::get('customer/types/{id}', [TypeController::class, 'show']);
 Route::post('customer/register', [CustomerController::class, 'register']);
 Route::post('customer/login', [CustomerController::class, 'login']);
 Route::get('customer/getuserbyusername/{username}', [CustomerController::class, 'getUserByUsername']);
+
+//dat phong
+Route::get('customer/available-room-types', [ReservationController::class, 'getAvailableRoomTypes']);
+Route::post('customer/reservations', [ReservationController::class, 'createReservation']);
